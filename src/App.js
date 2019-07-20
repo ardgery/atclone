411f8f7efee0ad74b7795d44bfa15b1d98bf5ebe
gery <ardinegery@gmail.com>
2019-07-20 16:48:26 +0700

    Commit_01

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './assets/styles/custom/style.css';
+import './assets/styles/base/style.css';
+import './assets/styles/style.css';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="p:r w:full h:full">
+        <div className="p:a m:a _t:0 _r:0 _b:0 _l:0 w:400 h:600 modal d:f f_d:c">
+          <h1 className="d:b f_w:700">Testo</h1>  
+          <div className="f:1 d:f">
+            <div className="col:2 m_r:20">
+              <p className="p_b:10">p</p>
+              <div className="b:b bg_c:violet"></div>
+            </div>
+            <div className="col:2 m_r:20">
+              <p className="p_b:10">p</p>
+              <div className="b:b bg_c:violet"></div>
+            </div>
+            <div className="col:2 m_r:20">
+              <p className="p_b:10">p</p>
+              <div className="b:b bg_c:violet"></div>
+            </div>
+            <div className="col:2 m_r:20">
+              <p className="p_b:10">p</p>
+              <div className="b:b bg_c:violet"></div>
+            </div>
+            <div className="col:2 m_r:20">
+              <p className="p_b:10">p</p>
+              <div className="b:b bg_c:violet"></div>
+            </div>
+            <div className="col:2">
+              <p className="p_b:10">p</p>
+              <div className="b:b bg_c:violet"></div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
